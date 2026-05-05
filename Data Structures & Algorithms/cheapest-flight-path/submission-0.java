dbac59a595422039f297f25c9e3e3ce877d05d9e
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2026-05-06 00:21:46 +0530

    Add: cheapest-flight-path - submission-0

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] dist = new int[n];
+        java.util.Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for (int i = 0; i <= k; i++) {
+            int[] temp = dist.clone();
+
+            for (int[] f : flights) {
+                int u = f[0], v = f[1], cost = f[2];
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + cost < temp[v]) {
+                    temp[v] = dist[u] + cost;
+                }
+            }
+
+            dist = temp;
+        }
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
