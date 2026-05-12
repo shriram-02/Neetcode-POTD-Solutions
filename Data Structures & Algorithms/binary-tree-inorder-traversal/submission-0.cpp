40f3acb4079e8701617f7d8e3bacdb186cb2a452
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2026-05-12 16:45:21 +0530

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st;
+
+        TreeNode* curr = root;
+
+        while (curr || !st.empty()) {
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            curr = st.top();
+            st.pop();
+
+            result.push_back(curr->val);
+
+            curr = curr->right;
+        }
+
+        return result;
+    }
+};
