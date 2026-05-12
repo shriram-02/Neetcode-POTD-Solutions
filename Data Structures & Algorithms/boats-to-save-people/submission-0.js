2effcd8ff1e2c81b699244f9c674447f6c8cbe97
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2026-05-12 16:52:04 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     */
+    numRescueBoats(people, limit) {
+        people.sort((a, b) => a - b);
+
+        let left = 0;
+        let right = people.length - 1;
+        let boats = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+
+            right--;
+            boats++;
+        }
+
+        return boats;
+    }
+}
