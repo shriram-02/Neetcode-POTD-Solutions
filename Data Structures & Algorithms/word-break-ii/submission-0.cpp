4e704bdf8f78298e7db2f6c977e5cd7cb1f8061c
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2026-05-12 16:43:09 +0530

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    unordered_set<string> dict;
+    unordered_map<int, vector<string>> memo;
+
+    vector<string> dfs(string& s, int start) {
+        if (memo.count(start))
+            return memo[start];
+
+        vector<string> result;
+
+        if (start == s.size()) {
+            result.push_back("");
+            return result;
+        }
+
+        for (int end = start + 1; end <= s.size(); end++) {
+            string word = s.substr(start, end - start);
+
+            if (dict.count(word)) {
+                vector<string> subSentences = dfs(s, end);
+
+                for (string sub : subSentences) {
+                    result.push_back(word + (sub.empty() ? "" : " " + sub));
+                }
+            }
+        }
+
+        return memo[start] = result;
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        dict = unordered_set<string>(wordDict.begin(), wordDict.end());
+        return dfs(s, 0);
+    }
+};
