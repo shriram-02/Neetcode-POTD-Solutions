5de92f7bf5bd1cad03497a909d85929a7e95d3cb
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-07-03 07:25:50 +0530

    Add: number-of-enclaves - submission-0

---
+++ Data Structures & Algorithms/number-of-enclaves/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                return
+            grid[i][j] = 0
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        for i in range(m):
+            if grid[i][0] == 1:
+                dfs(i, 0)
+            if grid[i][n - 1] == 1:
+                dfs(i, n - 1)
+
+        for j in range(n):
+            if grid[0][j] == 1:
+                dfs(0, j)
+            if grid[m - 1][j] == 1:
+                dfs(m - 1, j)
+
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                ans += grid[i][j]
+
+        return ans
