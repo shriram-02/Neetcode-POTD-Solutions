423ad43d79635763e258377f102dd9707b2c58ae
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-09-08 07:32:45 +0530

    Add: recover-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/recover-binary-search-tree/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        first = second = prev = None
+
+        def inorder(node):
+            nonlocal first, second, prev
+
+            if not node:
+                return
+
+            inorder(node.left)
+
+            if prev and prev.val > node.val:
+                if first is None:
+                    first = prev
+                second = node
+
+            prev = node
+            inorder(node.right)
+
+        inorder(root)
+
+        first.val, second.val = second.val, first.val
